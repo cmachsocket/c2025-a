4f325583ed69b06f36349c6ba7a4e4728e1f73af
cmachsocket <cmach_socket@outlook.com>
2025-09-01 17:38:10 +0800

    p2

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,19 @@
-#include <printf.h>
-
+#include <stdio.h>
+#define MAXN 10000005
+int pr[MAXN],tot,nopr[MAXN],n;
+void oula(){
+    for(int i=2;i<=MAXN;i++){
+        if(!nopr[i])pr[++tot]=i;
+        for(int j=1;j<=tot && i*pr[j]<=MAXN;j++){
+            nopr[i*pr[j]]=1;
+            if(!(i%pr[j]))break;
+        }
+    }
+}
 int main() {
-    printf("hello world!\n");
+    oula();
+    scanf("%d",&n);
+    printf(nopr[n]?"不是":"是");
+    printf("质数\n");
     return 0;
 }
